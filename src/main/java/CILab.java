b9f479371accbf54a3f26f939d185b1f2c5a60ae
NadavH12 <nadavhorowitz@gmail.com>
2023-06-07 12:12:28 -0700

    CILab and CILabTest Implementation Added

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -1,20 +1,85 @@
 public class CILab implements CILabInterface {
+	
     private String myString;
 
     @Override
     public String getString() {
+    	
+    	if(this.myString == null)
+    		return null;
+    	
         return myString;
     }
 
     @Override
     public void setString(String string) {
-        myString = string;
+        this.myString = string;
     }
-
+    
     @Override
     public boolean detectCapitalUse() {
+    	//get string to be examined
+    	String text = this.getString();
+    	
+    	//if string is null or length is 0, return false
+    	if (text == null || text.length() == 0)
+    		return false;
+    	
+    	//all caps case
+    	Boolean allCaps = checkIfAllCaps(text);
+    	if(allCaps)
+    		return true;
+    	
+    	//all lower case
+    	Boolean allLower = checkIfAllLower(text);
+    	if(allLower)
+    		return true;
+    	
+    	
+    	//firstLetter upper and rest lower case
+    	Boolean onlyFirstLetterCaps = checkIfOnlyFirstLetterCaps(text);
+    	if(onlyFirstLetterCaps)
+    		return true;
+    	
+    	
         return false;
+    }
+    
+    public boolean checkIfOnlyFirstLetterCaps(String text) {
+    	char firstLetter = text.charAt(0);
+    	
+    	if(Character.isLowerCase(firstLetter))
+    		return false;
+    	
+    	for(int i = 1; i < text.length(); i++) {
+    		char letter = text.charAt(i);
+    		if(Character.isUpperCase(letter))
+    			return false;
+    	}
+    	
+    	return true;	
+    }
+    
+    public boolean checkIfAllCaps(String text) {
+    	//check if string is all caps
+    	for(int i = 0; i < text.length(); i++) {
+    		char letter = text.charAt(i);
+    		if(Character.isLowerCase(letter)) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    public boolean checkIfAllLower(String text) {
+    	//check if string is all lowercase
+    	for(int i = 0; i < text.length(); i++) {
+    		char letter = text.charAt(i);
+    		if(Character.isUpperCase(letter)) {
+    			return false;
+    		}
+    	}
+    	return true;
     }
 
 }
-
--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CILabTest {
 
@@ -20,13 +21,30 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+    	myString.setString("leetcode");
+        boolean test = myString.detectCapitalUse();
+        assertTrue(test);
     }
+    
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+    	myString.setString("USA");
+    	boolean test = myString.detectCapitalUse();
+    	assertTrue(test);
     }
 
-
+    @Test
+    public void detectCapitalUseTest3() {
+    	myString.setString("Google");
+    	boolean test = myString.detectCapitalUse();
+    	assertTrue(test);
+    	
+    }
+    
+    @Test
+    public void detectCapitalUseTest4() {
+    	myString.setString("FalsE");
+    	boolean test = myString.detectCapitalUse();
+    	assertFalse(test);
+    }
 }
